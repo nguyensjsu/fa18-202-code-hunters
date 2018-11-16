d412ecd3a3591d90a5db3ccca9d6b110d779525c
HARSH <patelharsh9999@gmail.com>
2018-11-16 00:00:19 -0800

    Added intial code of game.

---
+++ project/GameOver.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+/**
+ * Write a description of class GameOver here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameOver extends Actor
+{
+/**
+ * GameOver - constructs the GameOver class
+ */
+    public GameOver(Score score)
+    {
+        GreenfootImage image = new GreenfootImage(100, 100);
+        image.setColor(new Color(0, 190, 50));
+        image.fillRect(0, 0, 100, 100);
+        image.setColor(Color.GREEN);
+        image.drawString("GAME OVER", 10, 25);
+        image.drawString("Score: " + score.score(), 10, 50);
+        setImage(image);
+    }
+}
