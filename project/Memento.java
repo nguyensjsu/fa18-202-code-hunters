a83a187cb07b883bd3b2b96e3338d6c088603b4a
HARSH <patelharsh9999@gmail.com>
2018-11-29 13:12:17 -0800

    added memento code

---
+++ project/Memento.java
@@ -0,0 +1,20 @@
+import greenfoot.*;
+/**
+ * Write a description of class Memento here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+
+public class Memento {
+   private World lastWorld;
+
+   public Memento(World lastWorld){
+      lastWorld = lastWorld;
+   }
+
+   public World getState(){
+      return lastWorld;
+   }	
+}
