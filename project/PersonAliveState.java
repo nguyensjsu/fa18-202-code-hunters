62bc1a71a2d6270ecd568fbe357d0a0a7dea0de3
Shridhar Saurin Shah <42818678+Shridhar-2205@users.noreply.github.com>
2018-11-30 10:47:00 -0800

    Update PersonAliveState.java

--- project/PersonAliveState.java
+++ project/PersonAliveState.java
@@ -1,3 +1,9 @@
+/**
+ * Write a description of class personAlive here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 public class PersonAliveState implements iPersonState  
 {
     public Person person;
